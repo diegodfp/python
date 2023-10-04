92102b75fdcd8ed2854d4cf71786e1f815b78998
diegodfp <diegodfp.fly@gmail.com>
2023-10-04 18:16:50 -0500

    correccionListas

--- talleres/Listas/nominaACME.py
+++ talleres/Listas/nominaACME.py
@@ -1,47 +1,134 @@
-##### FUNCIONES #######
-def leerInt(msj):
+def leerValHoraEmpl():
     while True:
         try:
-            n = int(input(msj))
-            if n < 1 or n>160:
-                print("Valor invalido. Tiene que estar entre 1 y")
+            n = int(input("Ingrese el valor de la hora trabajada del empleado: "))
+            if n < 8000 or n > 150000:
+                print("Valor de la Hora inválida. Debe estar en el rango de [8000, 150000]")
                 continue
             return n
         except ValueError:
-            print("error al ingresar el numero")
-def leerTexto(msj):
+            print("Error al ingresar el valor de la hora trabajada.")
+
+def leerHoraTrabEmpl():
     while True:
         try:
-            texto = input(msj)
-            if len(texto) < 1:
-                print("Recuerde que el texto no pude ir vacio")
-                continue
-            return texto
-        except ValueError:
-            print("error.")
-def leerFloat(msj):
-    while True:
-        try:
-            n = float(input(msj))
-            if n < 0:
-                print("nota invalida. Debe ser mayor o igual a 0")
+            n = int(input("Ingrese la horas trabajadas del empleado: "))
+            if n < 0 or n > 160:
+                print("Horas inválidas. Debe estar en el rango de [0, 160]")
                 continue
             return n
         except ValueError:
-            print("error al ingresar el numero")
+            print("Error al ingresar las horas.")
+
+def leerNombreEmpl():
+    while True:
+        try:
+            nom = input("Ingrese el nombre del empleado:")
+            nom = nom.strip()
+            if len(nom) == 0 or not nom.isalnum():
+                print("Nombre inválido")
+                continue
+            return nom
+        except Exception as e:
+            print("Error al ingresar el nombre.", e)
+
+def leerIDEmpl():
+    while True:
+        try:
+            n = int(input("Ingrese el ID del empleado: "))
+            if n < 1:
+                print("ID inválido. Debe ser mayor a cero")
+                continue
+            return n
+        except ValueError:
+            print("Error al ingresar el ID.")
+            
+def buscarEmpleado(lstEmpleado, idEmpl):
+    for i in range(len(lstEmpleado)):
+        if (lstEmpleado[i][0] == idEmpl):
+            return i
+    return -1
+def eliminarEmpleado(lstEmpleado):
+    print("\n\n2. Eliminar Empleado\n")
+    
+    idEmpl = leerIDEmpl()
+    posEmpl = buscarEmpleado(lstEmpleado, idEmpl)
+    if posEmpl == -1:
+        print("El código del empleado no existe.")
+        input()
+        return
+    lstEmpleado.pop(posEmpl)
+    print ("empleado borrado")
+    input("")
+def buscarporIDEmpleado(lstEmpleado):
+    idEmpl = leerIDEmpl()
+    posEmpl = buscarEmpleado(lstEmpleado, idEmpl)
+    if posEmpl == -1:
+        print("El código del empleado no existe.")
+        input()
+        return
+    print(lstEmpleado[posEmpl])
+    input("")
+def modificarEmpleado(lstEmpleado):
+    print("\n\n2. Modificar Empleado\n")
+    
+    
+    idEmpl = leerIDEmpl()
+    posEmpl = buscarEmpleado(lstEmpleado, idEmpl)
+    if posEmpl == -1:
+        print("El código del empleado no existe.")
+        input()
+        return
+    
+    print("\n")
+    while True:
+        op = int(input("\n1. Nombre\n2. Cantidad de Horas\n3. Valor de la hora\nOpcion? "))
+        if op < 1 or op > 3:
+            print("Opción inválida")
+            input()
+            continue
+        break
+    
+    if op == 1:
+        nombre = leerNombreEmpl()
+        lstEmpleado[posEmpl][1] = nombre
+    elif op == 2:
+        cantHoras = leerHoraTrabEmpl()
+        lstEmpleado[posEmpl][2] = cantHoras
+    elif op == 3:
+        valHora = leerValHoraEmpl()
+        lstEmpleado[posEmpl][3] = valHora
+
+def agregarEmpleado(lstEmpleado):
+    print("\n\n*** 1. Agregar empleado\n")
+    lstDatos = []
+    id = leerIDEmpl()
+    if buscarEmpleado(lstEmpleado, id) != -1:
+        print("El id ya existe en la lista")
+        input()
+        return
+    
+    lstDatos.append(id)
+    lstDatos.append(leerNombreEmpl())
+    lstDatos.append(leerHoraTrabEmpl())
+    lstDatos.append(leerValHoraEmpl())
+    
+    lstEmpleado.append(lstDatos)
+
 def menu():
     while True:
         try:
-            print("*** QUE DESEA HACER***")
-            print("1. Agregar Empleado")
-            print("2. Modificar Empleado")
-            print("3. Buscar Empleado")
-            print("4. Eliminar Empleado")
-            print("5. Listar Empleado")
-            print("6. Listar Nomina de un Empleado")
-            print("7. Listar Nomina de Todos los Empleados")
-            print("8. salir")
-            op = int(input(">>> Escoga una Opción (1-8)? "))
+            print("*** NOMINA ACME ***".center(40))
+            print("MENU".center(40))
+            print("1. Agregar empleado")
+            print("2. Modificar empleado")
+            print("3. Buscar emplado")
+            print("4. Eliminar empleado")
+            print("5. Listar empleados")
+            print("6. Listar nómina de un empleado")
+            print("7. Listar nómina de todos los empleados")
+            print("8. Salir")
+            op = int(input(">>> Opción (1-8)? "))
             if op < 1 or op > 8:
                 print("Opción no válida. Escoja de 1 a 8.")
                 input("Presione cualquier tecla para continuar...")
@@ -51,46 +138,32 @@ def menu():
             print("Opción no válida. Escoja de 1 a 8.")
             input("Presione cualquier tecla para continuar...")
 
-#####  MAIN PROGRAM #####
-lstEmpleados = []
-lstid = []
-lsthoras = []
-lstValor = []
-contadorID = 0
+def mostrarEmpleado(lstEmpleado):
+    print(lstEmpleado)
+    input("")
+## PROGRAMA PRINCIPAL
+lstEmpleado= []
 while True:
-    opcion = menu()
-    if opcion == 1:
-        print("\n\n1. Agregar Empleado")
-        contadorID +=1
-        lstid.append (contadorID)
-        lstEmpleados.append(leerTexto("nombre?"))
-        lsthoras.append(leerInt("horas trabajadas?"))
-        lstValor.append(leerFloat("valor de la hora?"))
-                    
-    elif opcion == 2: 
-        print("\n\n2. Modificar Campos de un empleado")
-        id = leerInt("ingrese el id del empleado que desea modificar")
-        lstEmpleados[id-1]= leerTexto("ingrese el nuevo nombre del empleado")
-        lsthoras[id-1]= leerInt("ingrese las nuevas horas del empleado")
-        lstValor[id-1]= leerFloat("ingrese el nuevo valor de las horas del empleado")
-    elif opcion == 3:
-        print("\n\n3. Eliminar Empleados")
-        id = leerInt("ingrese el id del empleado que desea modificar")
-        lstEmpleados.pop(id-1)
-        lsthoras.pop(id-1)
-        lstValor.pop(id-1)
-        lstid.pop(id-1)
-    elif opcion == 4:
-        print(lstid)
-        print(lstEmpleados)
-        print(lsthoras)
-        print(lstValor)
-    elif opcion == 8:
-        print("\n\nGracias por usar nuestro programa")
-        print("Adios")
-        input("Presione cualquier tecla para salir ...")
+    op = menu()
+    if op == 1:
+        agregarEmpleado(lstEmpleado)
+        print(lstEmpleado)
+        input()
+    elif op == 2:
+        modificarEmpleado(lstEmpleado)
+    elif op == 3:
+        buscarporIDEmpleado(lstEmpleado)
+    elif op == 4:
+        eliminarEmpleado(lstEmpleado)
+    elif op == 5:
+        mostrarEmpleado(lstEmpleado)
+    elif op == 6:
+        pass
+    elif op == 7:
+        pass
+    elif op == 8:
+        print("\n\nGracias por usar el software. Adios")
         break
-    input("Presione cualquier tecla para volver al MENU...")
 
 
 
