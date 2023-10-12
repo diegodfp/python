fcc15a142f11b3f732e4ba9f908d2ebbc3f1e709
Diego_Perez <diegodfp.fly@gmail.com>
2023-10-12 14:52:24 -0500

    tallerCrear

--- archivos/contarOrigent.py
+++ archivos/contarOrigent.py
@@ -1,30 +1,16 @@
-fd = open("/home/spukN01-019/Documents/Diego/python/archivos/mbox-short.txt","r")
+fd = open("C:\\Users\\DIEGO\\Documents\\Python\\python\\archivos\\mbox-short.txt","r")
+fdCorreos = open("C:\\Users\\DIEGO\\Documents\\Python\\python\\archivos\\correosEnviados.txt","w")
 
-def miFun(email):
-    return len(email)
 #guardamos los correos en un conjunto para que no cuente los correos repetidos
 setEmail = set()
-# for linea in fd:
-#     if linea.startswith("From:"): ###  buscamos lineas que comiencen con from
-#         #
-#         # email= linea.split()[1]
-#         # print(email)
-#         setEmail.add(linea.split()[1]) ## añadimos al conjunto las palabras que vayan despues del From
-
-
-# cl = (len(setEmail))
-# print("cantidad de correo de origenes distintos: ",cl)
-
-# for linea in fd:
-#     if linea.startswith("To:"):
-#         setEmail.add(linea.split()[1])
-# for email in sorted(setEmail, reverse=False, key= miFun):
-#     print(email, " enviado  [ok]")
-
 
 for linea in fd:
     if linea.startswith("To:"):
-        setEmail.add(linea.split()[1])
+        email = (linea.split()[1])
+        setEmail.add(email)
 for email in setEmail:
-    print(email, " enviado  [ok]")
+    mensaje = f"{email} enviado [ok]"
+    print (mensaje)
+    fdCorreos.write(mensaje+"\n")
 fd.close()
+fdCorreos.close()
